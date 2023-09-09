4fbdcfecdd9064c5e5fcce85a657cff2d67722c4
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-09 22:56:59 +0630

    Initialized

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>Navigation Bar</el-header>
+      <el-main>
+        <slot />
+      </el-main>
+    </el-container>
+  </div>
+</template>
+<style scoped></style>
+
+<script></script>
---
+++ pages/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <el-button type="primary">Primary</el-button>
+  </div>
+</template>
+
+<script>
+import {ElButton} from 'element-plus/dist/index.full.js';
+
+const components = {
+  ElButton
+};
+
+</script>
