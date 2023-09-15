e8f04a9a1ede7c1a1f8ca349cb06a9db732975a4
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-15 19:00:36 +0630

    GAME id updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,8 @@ export default defineNuxtConfig({
     ssr: false,
     app: {
         head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
             script: [
                 {
                     src: "/js/wavemoney.min.js"
