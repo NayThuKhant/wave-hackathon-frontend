4ae86d1b6d31108c0164ad539044ea1f4d463117
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-16 11:43:31 +0630

    Bypass added

--- composables/useLogin.js
+++ composables/useLogin.js
@@ -5,6 +5,8 @@ export default function () {
     const waveMoneySDK = useWaveMoneySDK()
     const {$axios} = useNuxtApp()
 
+
+    const route = useRoute()
     const config = useRuntimeConfig()
     const store = authStore()
     const loading = ref(true)
@@ -18,6 +20,12 @@ export default function () {
     }
     const login = async () => {
         const userInformation = await waveMoneySDK.getWaveUserInformation()
+
+        // TODO bypass the sdk on development
+        const msisdn = route.query.msisdn
+        if (msisdn) userInformation.msisdn = msisdn
+        console.log(userInformation)
+
         $axios.post(`${config.public.backendApi}/auth/login`, userInformation)
             .then((res) => {
                 storeResponseData(res)
