33c2f6bce1352fe3411150c6d0177c508e8d15a2
KhoonChoThar <khoonchothar98@gmail.com>
2023-09-16 23:43:28 +0630

    changed balance card heaader

--- pages/home.vue
+++ pages/home.vue
@@ -1,50 +1,76 @@
 <template>
   <!-- <TopHeader></TopHeader> -->
-  <div style="margin: 20px 20px 70px 20px">
-    <div class="addr-container">
+  <div style="background-color: #f7f7f7;">
+    <div class="top-header" :style="{ height: dynamicHeight }">
       <div class="header">
         <div style="flex: 1">
-          <el-image class="header-image" src="/images/ourlogo.png"></el-image>
+          <!-- <el-image class="header-image" src="/images/ourlogo.png"></el-image> -->
+          <p
+            style="
+              color: var(--grayscale-gray-50, #fff);
+              font-family: Manrope;
+              font-size: 12px;
+              font-style: normal;
+              font-weight: 600;
+              line-height: 150%; /* 18px */
+              margin: 0;
+            "
+          >
+            Welcome,
+          </p>
+          <p class="user-name">{{ user.name }}</p>
         </div>
         <div>
-          <el-image @click="goToProviders" src="/images/SearchProvidersIcon.svg"></el-image>
+          <el-image
+            @click="goToProviders"
+            src="/images/SearchProvidersIcon.svg"
+          ></el-image>
           <WaveCloseButton />
         </div>
       </div>
-<!--      <SearchBar></SearchBar>-->
       <div class="wave-balance-box">
-        <div style="display: flex">
+        <div class="balance-box">
+          <div>
+            <p class="balance-title">WavePay Balance</p>
+            <p class="balance-data">{{ balance }}</p>
+          </div>
           <el-image src="/images/WaveMoneyLogoV2.svg"></el-image>
         </div>
-        <div style="margin-left: 10px">
-          <p class="user-name">{{ user.name }}</p>
-          <p class="user-balance">{{ balance }}</p>
+        <div v-if="user.employee">
+          <el-divider style="margin: 0 auto"></el-divider>
+          <div>
+            <p class="balance-title">OnHold Balance</p>
+            <p class="balance-data">{{ balance }}</p>
+          </div>
         </div>
       </div>
+    </div>
+    <div :style="{ margin: dynamicMargin }">
+      <!--      <SearchBar></SearchBar>-->
       <Carousel></Carousel>
       <Services :providers="providers"></Services>
+      <el-alert
+        title="New Service Offer!"
+        type="success"
+        description="Checkout...You have got a new offer job!"
+        show-icon
+        style="
+          z-index: 999;
+          position: fixed;
+          bottom: 90px;
+          max-width: 440px;
+          padding: 20px 5px;
+          border-radius: 8px;
+          background: #fff;
+          box-shadow: 4px -4px 12px 0 rgba(0, 0, 0, 0.1),
+            -4px 4px 12px 0 rgba(0, 0, 0, 0.15);
+          color: #153051;
+          width: 95%;
+          right: 50%;
+          transform: translate(50%);
+        "
+      />
     </div>
-    <el-alert
-      title="New Service Offer!"
-      type="success"
-      description="Checkout...You have got a new offer job!"
-      show-icon
-      style="
-        z-index: 999;
-        position: fixed;
-        bottom: 90px;
-        max-width: 440px;
-        padding: 20px 5px;
-        border-radius: 8px;
-        background: #fff;
-        box-shadow: 4px -4px 12px 0 rgba(0, 0, 0, 0.1),
-          -4px 4px 12px 0 rgba(0, 0, 0, 0.15);
-        color: #153051;
-        width: 95%;
-        right: 50%;
-        transform: translate(50%);
-      "
-    />
     <el-drawer
       v-model="drawer"
       direction="btt"
@@ -91,25 +117,38 @@
 import { EditPen } from "@element-plus/icons-vue";
 import useHome from "~/composables/useHome";
 import useProvider from "~/composables/useProvider";
-import useWaveMoneySDK from "~/composables/useWaveMoneySDK"
+import useWaveMoneySDK from "~/composables/useWaveMoneySDK";
 import SamplePayment from "~/components/SamplePayment.vue";
 
 const balance = ref();
 const { providers, fetchProviders } = useProvider();
 
 onMounted(async () => {
-  const value = await useWaveMoneySDK().getWaveUserWalletBalance()
-  balance.value = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'MMK'
+  const value = await useWaveMoneySDK().getWaveUserWalletBalance();
+  balance.value = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "MMK",
   }).format(value);
 
   await fetchProviders();
 });
 
-
 const { user, drawer, goToProviders } = useHome();
+const dynamicHeight = computed(() => {
+  if (user.employee) {
+    return "70px";
+  } else {
+    return "85px";
+  }
+});
 
+const dynamicMargin = computed(() => {
+  if (user.employee) {
+    return "115px 20px 70px 20px";
+  } else {
+    return "50px 20px 70px 20px";
+  }
+});
 </script>
 
 <style lang="scss">
@@ -120,42 +159,69 @@ const { user, drawer, goToProviders } = useHome();
 .balance {
   font-weight: 900;
 }
-
+.top-header {
+  background-color: #153051;
+  padding: 12px 16px;
+}
 .header {
   display: flex;
   align-items: center;
 }
 
 .wave-balance-box {
-  display: flex;
-  align-items: center;
-  padding: 20px 16px;
-  background-color: #E9F2FF;
   border-radius: 8px;
-  margin: 10px 0 15px;
-
-  p {
-    margin: 0;
-    padding: 0;
-  }
-
-  .user-name {
-    color: #787878;
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 150%; /* 18px */
-  }
-
-  .user-balance {
-    color: #3188E1;
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 600;
-  }
+  background: #fff;
+  box-shadow: 0px 8px 10px 0px rgba(0, 0, 0, 0.05);
+  position: absolute;
+  right: 50%;
+  transform: translate(50%);
+  width: 100%;
+  max-width: 340px;
+  z-index: 1000;
+  margin: 10px 0;
+  padding: 12px 16px;
+}
+.balance-box {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.user-name {
+  line-height: 150%; /* 18px */
+  color: var(--grayscale-gray-50, #fff);
+  font-family: Manrope;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%; /* 21px */
+  margin: 5px 0;
 }
 
-.header-image  {
+.user-balance {
+  color: #3188e1;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+}
+.balance-title {
+  color: var(--grayscale-gray-500, #787878);
+  font-family: Manrope;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%; /* 18px */
+  margin: 0;
+}
+.balance-data {
+  color: var(--Light-Blue, #3188e1);
+  font-family: Manrope;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%; /* 27px */
+  margin: 0;
+}
+.header-image {
   margin-right: 16px;
   height: 48px;
   width: auto;
