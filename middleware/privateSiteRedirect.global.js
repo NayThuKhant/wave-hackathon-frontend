1d250941b021bd05b7dd8ce2241102b0dff24b82
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-16 13:07:33 +0630

    Home Redesigned

--- middleware/privateSiteRedirect.global.js
+++ middleware/privateSiteRedirect.global.js
@@ -1,8 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-    const secretKey = "teamguardiansofonenex"
+    const secretKey = "void"
     const key = localStorage.getItem("private_site_key") ?? prompt("Enter the secret key to access the private site")
 
     if (secretKey !== key) {
+        localStorage.removeItem("private_site_key")
         alert("You dont have access to enter this private site")
         return navigateTo('/')
     } else {
