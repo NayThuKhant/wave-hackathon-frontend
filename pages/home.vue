9fbc2870ee0889a4789a31a301d806975b36cb30
KhoonChoThar <khoonchothar98@gmail.com>
2023-09-16 20:44:01 +0630

    added alert box in home

--- pages/home.vue
+++ pages/home.vue
@@ -26,6 +26,28 @@
       <Carousel></Carousel>
       <Services :providers="providers"></Services>
     </div>
+    <el-alert
+      title="New Service Offer!"
+      type="success"
+      description="Checkout...You have got a new offer job!"
+      show-icon
+      style="
+        z-index: 999;
+        position: fixed;
+        bottom: 90px;
+        width: 100%;
+        max-width: 440px;
+        padding: 20px 5px;
+        border-radius: 8px;
+        background: #fff;
+        box-shadow: 4px -4px 12px 0px rgba(0, 0, 0, 0.1),
+          -4px 4px 12px 0px rgba(0, 0, 0, 0.15);
+        color: #153051;
+        width: 95%;
+        right: 50%;
+        transform: translate(50%);
+      "
+    />
     <el-drawer
       v-model="drawer"
       direction="btt"
