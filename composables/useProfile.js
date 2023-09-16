99537c4c870efaae23a86b1ee370405b8dda89ae
Nay Thu Khant <naythukhant644@gmail.com>
2023-09-16 09:30:28 +0630

    Search Provider Added

--- composables/useProfile.js
+++ composables/useProfile.js
@@ -1,37 +1,38 @@
 import {authStore} from "~/stores/authStore";
+
 export default function () {
     const {$axios} = useNuxtApp()
     const store = authStore();
     const config = useRuntimeConfig()
 
     const axiosHeaders = {
-        headers:{
-          Authorization: `Bearer ${localStorage.getItem('userToken')}`
+        headers: {
+            Authorization: `Bearer ${localStorage.getItem('userToken')}`
         }
     }
     const userProfile = store.getUserData
     const isActive = ref(userProfile.employee ? true : false)
-    
+
     const fetchMe = async () => {
         $axios.get(`${config.public.backendApi}/me`, axiosHeaders)
-            .then( (res) => {
+            .then((res) => {
                 store.setUserData(res.data.data)
                 isActive.value = true
-                navigateTo('employee-profile')
             })
     }
     const startWorking = () => {
         $axios.post(`${config.public.backendApi}/start-working`, {}, axiosHeaders)
-            .then( (response) => {
+            .then((response) => {
                 fetchMe()
+                navigateTo('employee-profile')
             })
     }
     const subscribeCategories = (data) => {
         $axios.put(`${config.public.backendApi}/subscribe-categories`, data, axiosHeaders)
-            .then( (response) => {
+            .then((response) => {
                 navigateTo('profile')
             })
     }
 
-    return { userProfile, startWorking, isActive, subscribeCategories }
+    return {userProfile, startWorking, isActive, subscribeCategories}
 }
